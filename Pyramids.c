e93582d79fd5d8e97fff8306642d0fb8d7833eb0
Kushika Sivaprakasam <63924175+kushika-s30@users.noreply.github.com>
2020-08-23 09:34:59 +0530

    Create Pyramids.c

---
+++ Pyramids.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+char x='*',new[10];
+int main(){
+    puts("Half pyramid with * :\n");
+    for(int i=0;i<6;i++){
+        new[i]=x;
+        puts(new);
+    }
+    
+    puts("\nHalf pyramid of numbers :\n");
+    for(int i=1;i<6;i++){
+        for(int j=1;j<=i;j++){
+            printf("%d",j);
+        }
+        printf("\n");
+    }
+    
+    puts("\nHalf pyramid of alphabets :\n");
+    for(char i='A';i<'F';i++){
+        for(char j='A';j<=i;j++){
+            printf("%c",j);
+        }
+        printf("\n");
+    }
+    
+    puts("\nInverted half pyramid of * :\n");
+    for(int i=6;i>=1;i--){
+        for(int j=1;j<=i;j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+    
+    puts("\nInverted half pyramid of * :\n");
+    
+}
