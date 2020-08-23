625f6a240ad8d7dbb6b1e42f5d693bb1b64adbe5
Kushika Sivaprakasam <63924175+kushika-s30@users.noreply.github.com>
2020-08-23 13:47:04 +0530

    Create FullPyramids.c

---
+++ FullPyramids.c
@@ -0,0 +1,22 @@
+puts("\nFull pyramid of numbers :\n");
+    k = 0;
+    int count = 0, count1 = 0;
+    for (int i = 1; i <= 6; ++i) {
+      for (int space = 1; space <= 6 - i; ++space) {
+         printf("  ");
+         ++count;
+        }
+      while (k != 2 * i - 1) {
+         if (count <= 5) {
+            printf("%d ", i + k);
+            ++count;
+         } else {
+            ++count1;
+            printf("%d ", (i + k - 2 * count1));
+         }
+         ++k;
+      }
+      count1 = count = k = 0;
+      printf("\n");
+   }
+    return 0;
