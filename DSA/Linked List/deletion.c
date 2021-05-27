17604793ee0a18ec4d82314b2689ec6af85ce841
Kushika Sivaprakasam <63924175+kushika-s30@users.noreply.github.com>
2021-05-27 09:09:28 +0530

    Update deletion.c

--- DSA/Linked List/deletion.c
+++ DSA/Linked List/deletion.c
@@ -53,6 +53,43 @@ void deleteNode(struct Node** head_ref, int key)
     free(temp); // Free memory
 }
  
+/* Given a reference (pointer to pointer) to the head of a list
+   and a position, deletes the node at the given position */
+void deleteNodepos(struct Node **head_ref, int position)
+{
+   // If linked list is empty
+   if (*head_ref == NULL)
+      return;
+ 
+   // Store head node
+   struct Node* temp = *head_ref;
+ 
+    // If head needs to be removed
+    if (position == 0)
+    {
+        *head_ref = temp->next;   // Change head
+        free(temp);               // free old head
+        return;
+    }
+ 
+    // Find previous node of the node to be deleted
+    for (int i=0; temp!=NULL && i<position-1; i++)
+         temp = temp->next;
+ 
+    // If position is more than number of nodes
+    if (temp == NULL || temp->next == NULL)
+         return;
+ 
+    // Node temp->next is the node to be deleted
+    // Store pointer to the next of node to be deleted
+    struct Node *next = temp->next->next;
+ 
+    // Unlink the node from linked list
+    free(temp->next);  // Free memory
+ 
+    temp->next = next;  // Unlink the deleted node from list
+}
+
 // This function prints contents of linked list starting
 // from the given node
 void printList(struct Node* node)
@@ -76,6 +113,7 @@ int main()
  
     puts("Created Linked List: ");
     printList(head);
+    deleteNodepos(&head, 0);
     deleteNode(&head, 1);
     puts("\nLinked List after Deletion of 1: ");
     printList(head);
